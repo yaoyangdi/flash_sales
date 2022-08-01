56d6739c5e7b2ba7125bb8024c83f8774a764975
Yaoyang Ding <dingyaoyang2019@gmail.com>
2022-08-01 21:50:20 +1000

    Home page slider section added

---
+++ frontend/ecommerce/src/assets/data.js
@@ -0,0 +1,80 @@
+import home from "../assets/images/home.webp"
+import school from "../assets/images/school.webp"
+
+import hoodie1 from "../assets/images/hoodie1.webp"
+import sweatshirt1 from "../assets/images/sweatshirt1.webp"
+import sweatshirt2 from "../assets/images/sweatshirt2.webp"
+import sweatshirt3 from "../assets/images/sweatshirt3.webp"
+import sweatshirt4 from "../assets/images/sweatshirt4.webp"
+import sweatshirt5 from "../assets/images/sweatshirt5.webp"
+import jacket1 from '../assets/images/jacket1.webp'
+import jacket2 from '../assets/images/jacket2.webp'
+
+export const sliderItems = [
+    {
+        id: 1,
+        title: "SUMMER FLASH SALES",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        img: home,
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        title: "SCHOOL SEASON\n"+"UP to 70% OFF",
+        desc: "GET SCHOLL READY WITH MORE SAVINGS!",
+        img: school,
+        bg: "fcf1ed",
+    }
+]
+
+
+export const popularProducts = [
+    {
+        id: 1,
+        img:hoodie1,
+        title: "Men Cartoon & Slogan Graphic Kangaroo Pocket Thermal Lined Drawstring Hoodie",
+        price: 21.95
+    },
+    {
+        id: 2,
+        img: sweatshirt1,
+        title: "Men Bear Print Thermal Lined Sweatshirt",
+        price: 25.95
+    },
+    {
+        id: 3,
+        img: sweatshirt2,
+        title: "ROMWE Guys Cartoon Bear Graphic Sweatshirt",
+        price: 21.95
+    },
+    {
+        id: 4,
+        img: sweatshirt3,
+        title: "Men Cartoon Face Print Sweatshirt",
+        price: 21.95
+    },
+    {
+        id: 5,
+        img: sweatshirt4,
+        title: "Men Letter Graphic Thermal Lined Sweatshirt",
+        price: 18.95,
+    },
+    {
+        id: 6,
+        img: sweatshirt5,
+        title: "Men Playing Card Print Sweatshirt",
+        price: 21.95
+    },
+    {
+        id: 7,
+        img: jacket1,
+        title: "Men 1pc Striped Drawstring Hooded 2 In 1 Jacket",
+        price: 23.95,
+    },
+    {
+        id: 8,
+        img: jacket2,
+        title: "Men Letter Patch Detail Two Tone Bomber Jacket",
+        price: 35.95,
+    }
+]
