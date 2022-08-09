8bddaf900bee2a4091ecef9094787722d349caca
Yaoyang Ding <dingyaoyang2019@gmail.com>
2022-08-09 20:48:05 +1000

    Swagger configured and API documentation created

--- backend/flashsales/src/main/java/com/example/flashsales/FlashsalesApplication.java
+++ backend/flashsales/src/main/java/com/example/flashsales/FlashsalesApplication.java
@@ -2,10 +2,18 @@ package com.example.flashsales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
+@EnableSwagger2
 public class FlashsalesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FlashsalesApplication.class, args);
 	}
 }
+
